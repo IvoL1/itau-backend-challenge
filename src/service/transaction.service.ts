edb9e6ec27d355e01505c45fe8bc92956eda3127
IvoL1 <ivoluis239@gmail.com>
2025-06-13 01:26:27 -0300

    fix: add .gitignore and remove ignored files

--- src/service/transaction.service.ts
+++ src/service/transaction.service.ts
@@ -12,19 +12,3 @@ export function calculateStats(transactions: Transaction[]): Statistics {
 
   return { count, sum, avg, max, min };
 }
-
-// export function calculateStats(transactions: Transaction[]): Statistics {
-//   const now = Date.now();
-//   const since = now - 60000; // 60 segundos atrás em milissegundos
-//   const filtered = transactions.filter((t) => new Date(t.dateTime).getTime() >= since);
-
-//   console.log("Filtered transactions:", filtered);
-
-//   const count = filtered.length;
-//   const sum = filtered.reduce((acc, t) => acc + t.value, 0);
-//   const avg = count ? sum / count : 0;
-//   const max = count ? Math.max(...filtered.map((t) => t.value)) : 0;
-//   const min = count ? Math.min(...filtered.map((t) => t.value)) : 0;
-
-//   return { count, sum, avg, max, min };
-// }
